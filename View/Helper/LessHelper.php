815903fb984d40bbee8fb46169f0c9f4f2bd6913
Brandon Ferens <brandon@perfectspace.com>
2014-01-08 15:55:19 -0800

    Switched Less compiler to utilize Lessc instead of Lessify. Seems to have better caching control.

--- View/Helper/LessHelper.php
+++ View/Helper/LessHelper.php
@@ -12,106 +12,146 @@
  * Mindthecode: http://www.mindthecode.com
  * Copyright 2011, Stef van den Ham
  *
+ * Perfect Space: http://perfectspace.com
+ * Copyright 2014, Brandon Ferens
+ *
+ * This code is based off of Stef van den Ham's Less plugin: https://github.com/Hyra/less
+ *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @author        Stef van den Ham
- * @copyright     Copyright 2011, Mindthecode (http://www.mindthecode.com)
+ * @author        Brandon Ferens
+ * @copyright     Copyright 2014, Perfect Space (http://www.perfectspace.com)
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 App::uses('Folder', 'Utility');
 App::uses('File', 'Utility');
 App::uses('Component', 'Controller');
-App::import('Vendor', 'Less.Lessify',
+App::import(
+  'Vendor',
+  'Less.Lessc',
   array(
-    'file' => 'lessphp' . DS . 'lessify.inc.php'
+    'file' => 'lessphp' . DS . 'lessc.inc.php'
   )
 );
 
 class LessHelper extends AppHelper {
 
-	public $helpers = array('Html');
+  public $helpers = array('Html');
 
-	public function __construct(View $View, $options = array()) {
-		parent::__construct($View, $options);
-		$this->lessFolder = new Folder(WWW_ROOT.'less');
-		$this->cssFolder = new Folder(WWW_ROOT.'css');
-	}
-	
-	/**
-	 * When called, will reset lessFolder for specific theme
-	 *
-	 * @note Note that it assumes themes are stored in app/View/Themed
-	 *
-	 * @param string $theme_name
-	 */
-	private function set_theme($theme_name) {
-  	
-    $app_root = ROOT . DS . APP_DIR;
+  public function __construct(View $View, $options = array()) {
+    parent::__construct($View, $options);
+    $this->lessFolder = new Folder(WWW_ROOT.'less');
+    $this->cssFolder = new Folder(WWW_ROOT.'css');
+  }
 
-    $theme_path = DS.$app_root.DS.'View'.DS.'Themed'.DS.$theme_name.DS.'webroot'.DS;
-		$this->lessFolder = new Folder($theme_path.'less');
-		$this->cssFolder = new Folder($theme_path.'css');
-  	
-	}
-	
-	public function css($file, $options = array()) {
-	
-	 if (isset($options['theme']) and trim($options['theme'])) {
-  	 $this->set_theme($options['theme']);
-	 }
-	 
-		if (is_array($file)) {
-			foreach ($file as $candidate) {
-				$source = $this->lessFolder->path.DS.$candidate.'.less';
-				$target = str_replace('.less', '.css', str_replace($this->lessFolder->path, $this->cssFolder->path, $source));
-				$this->auto_compile_less($source, $target);
-			}
-		} else {
-			if (isset($options['plugin']) and trim($options['plugin'])){
-				$this->lessFolder= new Folder(APP.'Plugin'.DS.$options['plugin'].DS.'webroot'.DS.'less');
-			}
-			$source = $this->lessFolder->path.DS.$file.'.less';
-			$target = str_replace('.less', '.css', str_replace($this->lessFolder->path, $this->cssFolder->path, $source));
-			$this->auto_compile_less($source, $target);
-		}
-		echo $this->Html->css($file);
-	}
+  /**
+   * When called, will reset lessFolder for specific theme
+   *
+   * @note Note that it assumes themes are stored in app/View/Themed
+   *
+   * @param string $theme_name
+   */
+  private function setTheme() {
 
-	public function auto_compile_less($lessFilename, $cssFilename) {
-		// Check if cache & output folders are writable and the less file exists.
-		if (!is_writable(CACHE.'less')) {
-			trigger_error(__d('cake_dev', '"%s" directory is NOT writable.', CACHE.'less'), E_USER_NOTICE);
-			return;
-		}
-		if (file_exists($lessFilename) == false) {
-			trigger_error(__d('cake_dev', 'File: "%s" not found.', $lessFilename), E_USER_NOTICE);
-			return;
-		}
+    $theme_path = App::themePath($this->theme) . 'webroot' . DS;
+    $this->lessFolder = new Folder($theme_path.'less');
+    $this->cssFolder = new Folder($theme_path.'css');
 
-		// Cache location
-		$cacheFilename = CACHE.'less'.DS.str_replace('/', '_', str_replace($this->lessFolder->path, '', $lessFilename).".cache");
+  }
 
-		// Load the cache
-		if (file_exists($cacheFilename)) {
-			$cache = unserialize(file_get_contents($cacheFilename));
-		} else {
-			$cache = $lessFilename;
-		}
+  public function css($file, $options = array()) {
 
-		$new_cache = Lessify::cexecute($cache);
-		if (!is_array($cache) || $new_cache['updated'] > $cache['updated'] || file_exists($cssFilename) === false) {
-			$cssFile = new File($cssFilename, true);
-			if ($cssFile->write($new_cache['compiled']) === false) {
-				if (!is_writable(dirname($cssFilename))) {
-					trigger_error(__d('cake_dev', '"%s" directory is NOT writable.', dirname($cssFilename)), E_USER_NOTICE);
-				}
-				trigger_error(__d('cake_dev', 'Failed to write "%s"', $cssFilename), E_USER_NOTICE);
-			}
+    if ($this->theme) {
+      $this->setTheme();
+    }
 
-			$cacheFile = new File($cacheFilename, true);
-			$cacheFile->write(serialize($new_cache));
-		}
-	}
+    if (is_array($file)) {
+      foreach ($file as $candidate) {
+        $source = $this->lessFolder->path.DS.$candidate.'.less';
+        $target = str_replace('.less', '.css', str_replace($this->lessFolder->path, $this->cssFolder->path, $source));
+        $this->autoCompileLess($source, $target, $options);
+      }
+    } else {
+      $source = $this->lessFolder->path.DS.$file.'.less';
+      $target = str_replace('.less', '.css', str_replace($this->lessFolder->path, $this->cssFolder->path, $source));
+      $this->autoCompileLess($source, $target, $options);
+    }
+    echo $this->Html->css($file);
+  }
 
+  public function autoCompileLess($lessFilename, $cssFilename, $options = array()) {
+    // Check if cache & output folders are writable and the less file exists.
+    if (!is_writable(CACHE.'less')) {
+      trigger_error(__d('cake_dev', '"%s" directory is NOT writable.', CACHE.'less'), E_USER_NOTICE);
+      return;
+    }
+    if (file_exists($lessFilename) == false) {
+      trigger_error(__d('cake_dev', 'File: "%s" not found.', $lessFilename), E_USER_NOTICE);
+      return;
+    }
+
+    // Set the proper compression type. Defaults to 'compressed'
+    $compressed = 'compressed';
+    if (isset($options['compressed'])) {
+      switch ($options['compressed']) {
+        case 'lessjs':
+          $compressed = 'lessjs';
+          break;
+        case 'classic':
+          $compressed = 'classic';
+          break;
+        case null:
+          $compressed = null;
+          break;
+        default:
+          $compressed = 'compressed';
+          break;
+      }
+    }
+
+    // Set if less should be cached. Defaults to 'true'
+    $isCached = true;
+    if (isset($options['cache'])) {
+      switch ($options['cache']) {
+        case false:
+          $isCached = false;
+          break;
+        default:
+          $isCached = true;
+          break;
+      }
+    }
+
+    $less = new lessc;
+    $less->setFormatter($compressed);
+
+    if ($isCached) {
+      // Cache location
+      $cacheFilename = CACHE . 'less' . DS .str_replace('/', '_', str_replace($this->lessFolder->path, '', $lessFilename) . ".cache");
+
+      // Load the cache
+      if (file_exists($cacheFilename)) {
+        $cache = unserialize(file_get_contents($cacheFilename));
+      } else {
+        $cache = $lessFilename;
+      }
+
+      $new_cache = $less->cachedCompile($cache);
+
+      if (!is_array($cache) || $new_cache['updated'] > $cache['updated'] || file_exists($cssFilename) === false) {
+        $cssFile = new File($cssFilename, true);
+        if ($cssFile->write($new_cache['compiled']) === false) {
+          if (!is_writable(dirname($cssFilename))) {
+            trigger_error(__d('cake_dev', '"%s" directory is NOT writable.', dirname($cssFilename)), E_USER_NOTICE);
+          }
+          trigger_error(__d('cake_dev', 'Failed to write "%s"', $cssFilename), E_USER_NOTICE);
+        }
+        $cacheFile = new File($cacheFilename, true);
+        $cacheFile->write(serialize($new_cache));
+      }
+    } else {
+      $less->checkedCompile($lessFilename, $cssFilename);
+    }
+  }
 }
